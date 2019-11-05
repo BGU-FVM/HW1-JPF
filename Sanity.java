93617cb5f5bd314676097d063ad987766bfa20ac
Gera Weiss <gera.weiss@gmail.com>
2019-11-05 18:21:05 +0200

    Update Sanity.java

--- Sanity.java
+++ Sanity.java
@@ -5,17 +5,17 @@ public class Sanity {
     public static void main( String args[] ) {
         System.out.println("Sanity started");
         Random r = new Random();
-        String researcherString = "Hoare,Pnueli,Clarke,Emerson,Sifakis,Lamport,,";
-        String[] researchers = researcherString.split(",");
-        int commCount = countCommas(researcherString);
+        String researcherString = "Hoare Pnueli Clarke Emerson Sifakis Lamport ";
+        String[] researchers = researcherString.split(" ");
+        int wordCount = countWords(researcherString);
         
-        System.out.println(researchers[r.nextInt(commCount)]);
+        System.out.println(researchers[r.nextInt(wordCount)]);
     }
 
-    private static int countCommas(String s) {
-        int count = 0;
+    private static int countWords(String s) {
+        int count = 1;
         for ( char c : s.toCharArray() ) {
-            if ( c == ',' ) count++;
+            if ( c == ' ' ) count++;
         }
         return count;
     }
